2fc1277a095856a44623272b6457d71599d13358
lukeloken <lukeloken@gmail.com>
2018-01-28 12:00:01 -0600

    Script to note important packages for course and spatial analysis

---
+++ R/PackageInstall.R
@@ -0,0 +1,13 @@
+#Packages to install for spatial analyses
+
+library(rgdal)
+library(rgeos)
+library(sp)
+library(maps)
+library(raster)
+library(maptools)
+
+#Packages to install for rmarkdown
+
+library(rmarkdown)
+library(knitr)
